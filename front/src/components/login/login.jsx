4f06ecdebd5cde80e49759a991288f3ba1e87533
swchae516 <swchae516@gmail.com>
2022-01-20 11:18:03 +0900

    modify login page

--- front/src/components/login/login.jsx
+++ front/src/components/login/login.jsx
@@ -1,21 +1,37 @@
 import React from 'react';
 import styles from './login.module.css';
+import { FcGoogle } from 'react-icons/fc';
+import { SiNaver } from 'react-icons/si';
+import { RiKakaoTalkFill } from 'react-icons/ri';
 
 const Login = props => (
   <>
     <section className={styles.section}>
-      <div>
-        <h1>로그인</h1>
-        <ul>
-          <li>
-            <button>Google</button>
+      <div className={styles.login}>
+        <h1 className={styles.h1}>로그인</h1>
+        <ul className={styles.list}>
+          <li className={styles.item}>
+            <button className={styles.google}>
+              <FcGoogle className={styles.icon} />
+              Google 계정으로 로그인
+            </button>
           </li>
-          <li>
-            <button>카카오</button>
+          <li className={styles.item}>
+            <button className={styles.naver}>
+              <SiNaver className={styles.icon} />
+              Naver 계정으로 로그인
+            </button>
           </li>
-          <li>
-            <button>네이버</button>
+          <li className={styles.item}>
+            <button className={styles.kakao}>
+              <RiKakaoTalkFill className={styles.icon} />
+              Kakao 계정으로 로그인
+            </button>
           </li>
+          <hr className={styles.hr} />
+          <span>
+            해줘잉이 처음이신가요? <a href="#">회원가입</a>
+          </span>
         </ul>
       </div>
     </section>
