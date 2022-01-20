0172fd749b64696006f713caa4063b872b02e511
swchae516 <swchae516@gmail.com>
2022-01-20 15:27:00 +0900

    add newProfile page

--- front/src/app.jsx
+++ front/src/app.jsx
@@ -2,12 +2,14 @@ import './app.css';
 import Footer from './components/footer/footer';
 import Header from './components/header/header';
 import Login from './components/login/login';
+import NewProfile from './components/profile/newProfile';
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <Login />
+      {/* <Login /> */}
+      <NewProfile />
       <Footer />
     </div>
   );
