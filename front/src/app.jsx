c1df7bb90c007c80cc4c6fb088f8f9cb83d34f00
swchae516 <swchae516@gmail.com>
2022-01-21 16:17:34 +0900

    add profile page

--- front/src/app.jsx
+++ front/src/app.jsx
@@ -5,21 +5,17 @@ import Login from './components/login/login';
 import NewProfile from './components/profile/newProfile';
 import { Link, Route, Switch } from 'react-router-dom';
 import MdProfile from './components/profile/mdProfile';
+import Profile from './components/profile/profile';
 
 function App() {
   return (
     <div className="App">
       <Header />
       <Switch>
-        <Route exact path="/">
-          <Login />
-        </Route>
-        <Route exact path="/newProfile">
-          <NewProfile />
-        </Route>
-        <Route exact path="/mdProfile">
-          <MdProfile />
-        </Route>
+        <Route exact path="/" component={Login} />
+        <Route exact path="/profile" component={Profile} />
+        <Route exact path="/mdProfile" component={MdProfile} />
+        <Route exact path="/newProfile" component={NewProfile} />
       </Switch>
       <Footer />
     </div>
