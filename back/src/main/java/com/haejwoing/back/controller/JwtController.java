5e0bc5dbb58342632a924bdc1f9b15edaf6d56e1
재현 <jungjh122@naver.com>
2022-02-03 17:30:00 +0900

    유저 정보 반환

--- back/src/main/java/com/haejwoing/back/controller/JwtController.java
+++ back/src/main/java/com/haejwoing/back/controller/JwtController.java
@@ -14,7 +14,7 @@ import java.time.LocalDateTime;
 import java.util.Map;
 
 @RestController
-@RequestMapping("/oauth/jwt")
+@RequestMapping("/jwt")
 public class JwtController {
 
     @Autowired
--- back/src/main/java/com/haejwoing/back/controller/UserController.java
+++ back/src/main/java/com/haejwoing/back/controller/UserController.java
@@ -7,6 +7,7 @@ import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -23,14 +24,16 @@ public class UserController {
 
     @ApiOperation(value = "회원정보")
     @GetMapping("/{nickname}")
-    public ResponseEntity<Map<String, Object>> userInfo(@RequestBody @ApiParam(value = "유저 정보") String nickname){
+    public ResponseEntity<Map<String, Object>> userInfo(@PathVariable @ApiParam(value = "유저 정보") String nickname){
         log.info("회원정보 ");
         log.info("{}",nickname);
+        HttpStatus status = HttpStatus.ACCEPTED;
 
         Map<String, Object> result = new HashMap<>();
         User user = userService.searchByUsername(nickname);
+        System.out.println(user);
         result.put("info", user);
 
-        return null;
+        return new ResponseEntity<Map<String, Object>>(result, status);
     }
 }
