8f7fcc8fb8c619fb35ac83d6bc15292ee1b4dbfd
yoonheehan <grayman1234@naver.com>
2022-02-05 22:11:47 +0900

    02_04_03

--- back/src/main/java/com/haejwoing/back/model/dto/Board.java
+++ back/src/main/java/com/haejwoing/back/model/dto/Board.java
@@ -3,16 +3,26 @@ package com.haejwoing.back.model.dto;
 
 import lombok.Data;
 import nonapi.io.github.classgraph.json.Id;
+import springfox.documentation.spring.web.json.Json;
+
+import javax.swing.*;
+import java.sql.Timestamp;
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Map;
 
 @Data
 public class Board {
 
     private int idboard;
-    private String title;
     private String view_range;
     private String content;
     private String board_image;
     private int type;
-    private int vote_idvote;
+
+    private LocalDateTime created_at;
+    private LocalDateTime updated_at;
+    private LocalDateTime due_date;
+    private String vote_contents;
 
 }
