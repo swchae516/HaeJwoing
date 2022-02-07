18b29cd86631964ddbbb2d0295ba59dfa1597f73
재현 <jungjh122@naver.com>
2022-02-07 11:46:11 +0900

    0207

--- back/src/main/java/com/haejwoing/back/model/dto/User.java
+++ back/src/main/java/com/haejwoing/back/model/dto/User.java
@@ -1,9 +1,7 @@
 package com.haejwoing.back.model.dto;
 
-import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 
 @Data
 @Builder
@@ -16,4 +14,5 @@ public class User {
     private String image;
     private String role;
     private int userStatus;
+    private double point;
 }
--- back/src/main/java/com/haejwoing/back/model/service/FileService.java
+++ back/src/main/java/com/haejwoing/back/model/service/FileService.java
@@ -2,7 +2,10 @@ package com.haejwoing.back.model.service;
 
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
+import java.util.List;
+
 public interface FileService {
 
-    void fileInsert(MultipartFile[] file);
+    List<String> fileInsert(MultipartFile[] file) throws IOException;
 }
